0744c8c492ece04b523752924a77d475a30bd984
Daniel Zema <fenton@chalmers.se>
2026-01-01 15:27:11 +0100

    implemented function to find a word in the beginning of a string

--- Main.hs
+++ Main.hs
@@ -1,10 +1,23 @@
 module Main where 
 
+-- Parse a single char
 charParser :: Char -> String -> Maybe (String, Char)
 charParser goal text =  
     case text of 
         x:xs | x == goal -> Just (xs, goal)
         _             -> Nothing
+
+-- Check if a word is in the beginning of a string
+stringParser :: String -> String -> Maybe (String, String)
+stringParser [] text = Just (text, [])
+stringParser (firstGoal:restGoal) text = 
+    case text of 
+        (firstInput:restInput) | firstInput == firstGoal -> case stringParser restGoal restInput of 
+                                Nothing -> Nothing 
+                                Just (remainder, restResult) -> Just (remainder, firstGoal : restResult)
+        _ -> Nothing
+
+
 
 main :: IO ()
 main = undefined
